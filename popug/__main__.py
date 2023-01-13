05d1cfc3b3f3701a69d303d36858291fc54a8f4a
D.A.Bashkirtsev <bashkirtsevich@users.noreply.github.com>
2023-01-13 23:51:08 +0500

    Update __main__.py

--- popug/__main__.py
+++ popug/__main__.py
@@ -1,9 +1,16 @@
+import os
 import sys
 
 __all__ = ('main',)
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
 def main():
+    with open(os.path.join(__location__, "parrot.txt")) as f:
+        for s in f:
+            print(s.rstrip())
+
     for i, arg in enumerate(sys.argv):
         print("Arg:", i, "\tvalue:", arg)
 
